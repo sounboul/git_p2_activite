bd88ed3c915e2eaedced339d29c1462fcb4cf01a
sounboul <sounboul@hotmail.fr>
2017-02-10 10:40:34 +0000

    Modif script test

--- test.php
+++ test.php
@@ -1,7 +1,17 @@
 <?php 
-	$b = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"];
+	//inclure le fichier contenant la fonction
+	include("solution.php");
+
+	//Similer les tableaux $b (codelivre nbpag) et $c(catégories)
+ 	$b = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"];
  	$c = ["A", "B", "C", "D"];
+    
+    //Résultat attendu du test
     $res = "(A : 0) - (B : 1290) - (C : 515) - (D : 600)";
+
+	//comparaison des deux résultats
    	echo stockList($b, $c);
-   	echo "<br>Compaire to : <br>".$res;
+   	echo "<br>Compaire to : <br>".$res
+
+	//******fin test********/
  ?>
